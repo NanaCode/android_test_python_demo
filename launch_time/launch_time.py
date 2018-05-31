5ff3bf5788ec77e6f395bbfef13f376bec3c8964
NanaCode <2503605326@qq.com>
2018-05-31 22:41:54 +0800

    func of class App

--- launch_time/launch_time.py
+++ launch_time/launch_time.py
@@ -2,16 +2,32 @@
 __author__ = 'Nana'
 __date__ = '2018/5/31 22:22'
 
+import os
 
+
+# app类
 class App(object):
+    def __init__(self):
+        self.content = ""
+        self.start_time = 0
+
+    # 启动App
     def launch_app(self):
-        pass
+        cmd = "adb shell am start -W -n com.android.browser/.BrowserActivity"
+        self.content = os.popen(cmd)
 
+    # 停止App
     def stop_app(self):
-        pass
+        cmd = "adb shell am force-stop com.android.browser"
+        os.popen(cmd)
 
+    # 获取启动时间
     def get_launched_time(self):
-        pass
+        for line in self.content.readlines:
+            if "ThisTime" in line:
+                self.start_time = line.split(":")[1]
+                break
+            return self.start_time
 
 
 class Controller(object):
